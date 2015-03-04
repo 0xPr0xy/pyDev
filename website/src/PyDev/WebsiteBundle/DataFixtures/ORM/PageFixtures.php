33cae116ab8a52f502d2ccb3fc124f1633291910
Peter IJlst <peterijlst@Peters-MacBook-Pro-2.local>
2015-03-04 18:11:15 +0100

    remove kunstmaan fixtures, add custom fixtures

---
+++ website/src/PyDev/WebsiteBundle/DataFixtures/ORM/PageFixtures.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace PyDev\WebsiteBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Kunstmaan\MediaBundle\Helper\Services\MediaCreatorService;
+use Kunstmaan\NodeBundle\Helper\Services\PageCreatorService;
+use Kunstmaan\PagePartBundle\Helper\Services\PagePartCreatorService;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use PyDev\WebsiteBundle\Entity\Pages\HomePage;
+
+/**
+ * PageFixtures
+ */
+class PageFixtures extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * Username that is used for creating pages
+     */
+    const ADMIN_USERNAME = 'admin';
+
+    private $languages = ['en'];
+
+    private $translateMePrefix = 'TRANSLATE_ME ';
+
+    /** @var ContainerInterface */
+    private $container = null;
+
+    /** @var ObjectManager */
+    private $manager;
+
+    /** @var PageCreatorService */
+    private $pageCreator;
+
+    /** @var PagePartCreatorService */
+    private $pagePartCreator;
+
+    /** @var MediaCreatorService */
+    private $mediaCreator;
+    
+    /**
+     * Load data fixtures with the passed EntityManager.
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+
+        $this->pageCreator     = $this->container->get('kunstmaan_node.page_creator_service');
+        $this->pagePartCreator = $this->container->get('kunstmaan_pageparts.pagepart_creator_service');
+        $this->mediaCreator    = $this->container->get('kunstmaan_media.media_creator_service');
+        
+        $this->createPages();
+    }
+    
+    /**
+     * Create pages
+     */
+    private function createPages()
+    {
+        $homePage = new HomePage();
+        $homePage->setTitle('Home');
+
+        $translations = array();
+        $translations[] = array('language' => 'en', 'callback' => function($page, $translation, $seo) {
+            $translation->setTitle('Home');
+            $translation->setSlug('');
+        });
+
+        $options = array(
+            'parent' => null,
+            'page_internal_name' => 'homepage',
+            'set_online' => true,
+            'hidden_from_nav' => false,
+            'creator' => self::ADMIN_USERNAME
+        );
+
+        $this->pageCreator->createPage($homePage, $translations, $options);
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return int
+     */
+    public function getOrder()
+    {
+        return 200;
+    }
+
+    /**
+     * Sets the Container.
+     *
+     * @param ContainerInterface $container A ContainerInterface instance
+     *
+     * @api
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+}
